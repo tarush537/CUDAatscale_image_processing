aa64a4b4a5ece062e1827969b3039a9262fd25f9
tarush537 <119066646+tarush537@users.noreply.github.com>
2024-07-25 12:38:57 +0530

    Add files via upload

---
+++ Include/main.cuh
@@ -0,0 +1,21 @@
+//
+// Created by guidocalvano on 1/27/23.
+//
+
+#ifndef CUDAATSCALEFORTHEENTERPRISECOURSEPROJECTTEMPLATE_MAIN_CUH
+#define CUDAATSCALEFORTHEENTERPRISECOURSEPROJECTTEMPLATE_MAIN_CUH
+#include <stdio.h>
+#include <cuda.h>
+#include <npp.h>
+// #include <ImageIO.h>
+// #include <ImagesCPU.h>
+#include <nppi.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <dirent.h>
+#include <boost/algorithm/string/predicate.hpp>
+
+
+#endif //CUDAATSCALEFORTHEENTERPRISECOURSEPROJECTTEMPLATE_MAIN_CUH
